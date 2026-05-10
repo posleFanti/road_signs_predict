60822267f6b90dc7f26172708200471cfa5ce1b8
Alex <boxalex2014@gmail.com>
2026-05-10 10:18:18 +0300

    yolo26

--- train_yolo26n.py
+++ train_yolo26n.py
@@ -1,11 +1,33 @@
+import os
+import cv2
+
+# Отключаем конфликты потоков
+#cv2.setNumThreads(0)
+#os.environ["OMP_NUM_THREADS"] = "1"
+#os.environ["MKL_NUM_THREADS"] = "1"
+
+# МАГИЧЕСКАЯ СТРОКА: Заставляем PyTorch не использовать /dev/shm
+#os.environ["PYTORCH_MULTIPROCESSING_SHARING_STRATEGY"] = "file_system"
+
+# Отключаем облачные логи
+os.environ["WANDB_MODE"] = "disabled"
+
 from ultralytics import YOLO
 
-model = YOLO('yolo26n.pt')
+if __name__ == '__main__':
+    model = YOLO('yolo26n.pt')
 
-results = model.train(
-    data='data_ru.yaml',
-    imgsz=640,
-    epochs=25,
-    batch=48,
-    name='yolo26n_rtsd_dataset',
-)
+    results = model.train(
+        data='data_ru.yaml',
+        imgsz=640,
+        epochs=50,
+        patience=20,
+        #batch=12,             # Если упадет, снизьте до 4
+        name='yolo26n_rtsd_dataset',
+        #workers=12,           # Ставим 2 для проверки (это уже ускорит в 2-3 раза)
+        #cache=False,         # КРИТИЧНО: не кэшировать датасет в RAM
+        amp=True,            # Mixed precision (ускоряет и экономит память)
+        optimizer='MuSGD'
+        #deterministic=False  # Отключаем жесткую детерминированность
+        #resume=True
+    )
--- train_yolo26s.py
+++ train_yolo26s.py
@@ -20,12 +20,14 @@ if __name__ == '__main__':
     results = model.train(
         data='data_ru.yaml',
         imgsz=640,
-        epochs=25,
+        epochs=50,
+        patience=20,
         #batch=12,             # Если упадет, снизьте до 4
         name='yolo26s_rtsd_dataset',
         #workers=12,           # Ставим 2 для проверки (это уже ускорит в 2-3 раза)
         #cache=False,         # КРИТИЧНО: не кэшировать датасет в RAM
         amp=True,            # Mixed precision (ускоряет и экономит память)
+        optimizer='MuSGD'
         #deterministic=False  # Отключаем жесткую детерминированность
         #resume=True
     )
